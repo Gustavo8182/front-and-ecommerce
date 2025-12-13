4c0d95af08ef760a174301416af3e2bc21e7ca36
Gustavo Alves <gustavo_rodrigo@id.uff.br>
2025-12-13 14:02:04 -0300

    Config: Apontando API para Produção (Render)

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,4 +1,10 @@
-export const environment = {
+/*export const environment = {
   production: false,
   apiBaseUrl: 'http://localhost:8080' // A fonte da verdade única!
+};*/
+
+export const environment = {
+  production: true,
+  // IMPORTANTE: Sem a barra '/' no final
+  apiBaseUrl: 'https://vendas-api-prod.onrender.com'
 };
