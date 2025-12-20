971889a299b076c5c1cbc237b0658eac603be889
Gustavo Alves B <66348518+Gustavo8182@users.noreply.github.com>
2025-12-19 23:19:12 -0300

    Switch environment to development configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,8 @@
-/*
 export const environment = {
   production: true,
   // IMPORTANTE: Sem a barra '/' no final
   apiBaseUrl: 'https://vendas-api-prod.onrender.com'
 };
-*/
 
 
-export const environment = {
-  production: false,
-  apiBaseUrl: 'http://localhost:8080' // A fonte da verdade única!
-};
+
